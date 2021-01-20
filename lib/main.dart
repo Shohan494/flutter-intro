3f3b659bba8330ff16e71e8856c4164d8be9e887
Shohanul Alam <shohan12224943@gmail.com>
2021-01-21 02:14:31 +0600

    images shorthands

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,10 @@ class Home extends StatelessWidget {
       ),
       body: Center(
           child: Image(
-              image: NetworkImage('https://scontent.fdac7-1.fna.fbcdn.net/v/t1.0-9/18119260_1591122740928692_438563486387663108_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=b7ebzyg1We4AX-bnD5B&_nc_ht=scontent.fdac7-1.fna&oh=c0689c7e3cd55ce4ab8ebb2fdec4f298&oe=602D128C')
+              image: AssetImage('assets/shohan.jpg')
           ),
+        // shortcut after child: Image.asset('')
+        // shortcut after child: Image.network('')
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
