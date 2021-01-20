8ce6f7bd1149bc1e9452f819652666be22c05516
Shohanul Alam <shohan12224943@gmail.com>
2021-01-21 02:04:16 +0600

    network image added

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  )); // ROOT WIDGET home:
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-          child: Text(
-              'Hello from Shohan',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.blueGrey[600],
-              fontFamily: 'Pacifico'
-            ),
-          )
+          child: Image(
+              image: NetworkImage('https://scontent.fdac7-1.fna.fbcdn.net/v/t1.0-9/18119260_1591122740928692_438563486387663108_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=b7ebzyg1We4AX-bnD5B&_nc_ht=scontent.fdac7-1.fna&oh=c0689c7e3cd55ce4ab8ebb2fdec4f298&oe=602D128C')
+          ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click'),
+        child: Text('Click Me! '),
         backgroundColor: Colors.red[600],
       ),
-    )
-  )); // ROOT WIDGET home:
+    );
+  }
 }
